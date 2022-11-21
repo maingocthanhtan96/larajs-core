1ef5bec70fa3561153f4f77d2ee573e62a7cb2d5
tanmnt <maingocthanhtan96@gmail.com>
2022-11-21 22:33:30 +0700

    first commit

---
+++ src/GeneratorProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaraJS\Generator;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+
+    }
+}
