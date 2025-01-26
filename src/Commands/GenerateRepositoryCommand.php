4ca6d877b145eee41d2151233077474db6a3be7f
tanmnt <maingocthanhtan96@gmail.com>
2025-01-26 09:38:42 +0700

    feat: make repository console

---
+++ src/Commands/GenerateRepositoryCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace LaraJS\Core\Commands;
+
+class GenerateRepositoryCommand extends GeneratorCommand
+{
+    protected $signature = 'larajs:make:repository
+                            {name : The name of the repository}';
+
+    protected $description = 'Generate a new repository';
+
+    protected string $successMessage = 'Repository created successfully!';
+
+    protected string $alreadyExistsMessage = 'Repository already exists!';
+
+    protected function directoryPath(): string
+    {
+        return app_path("Repositories/{$this->argument('name')}");
+    }
+
+    protected function generateFiles(): void
+    {
+        $this->generateReadRepository();
+        $this->generateReadInterfaceRepository();
+        $this->generateWriteRepository();
+        $this->generateWriteInterfaceRepository();
+    }
+
+    private function generateReadRepository(): void
+    {
+        $this->generateFile(
+            'read.repository',
+            [
+                'name' => $this->argument('name'),
+            ],
+            "{$this->directoryPath()}/{$this->argument('name')}ReadRepository.php"
+        );
+    }
+
+    private function generateReadInterfaceRepository(): void
+    {
+        $this->generateFile(
+            'read.repository.interface',
+            [
+                'name' => $this->argument('name'),
+            ],
+            "{$this->directoryPath()}/{$this->argument('name')}ReadRepositoryInterface.php"
+        );
+    }
+
+    private function generateWriteRepository(): void
+    {
+        $this->generateFile(
+            'write.repository',
+            [
+                'name' => $this->argument('name'),
+            ],
+            "{$this->directoryPath()}/{$this->argument('name')}WriteRepository.php"
+        );
+    }
+
+    private function generateWriteInterfaceRepository(): void
+    {
+        $this->generateFile(
+            'write.repository.interface',
+            [
+                'name' => $this->argument('name'),
+            ],
+            "{$this->directoryPath()}/{$this->argument('name')}WriteRepositoryInterface.php"
+        );
+    }
+}
--- src/LaraJSCoreServiceProvider.php
+++ src/LaraJSCoreServiceProvider.php
@@ -5,6 +5,7 @@ namespace LaraJS\Core;
 use Illuminate\Support\ServiceProvider;
 use LaraJS\Core\Commands\GenerateActionCommand;
 use LaraJS\Core\Commands\GenerateControllerCommand;
+use LaraJS\Core\Commands\GenerateRepositoryCommand;
 use LaraJS\Core\Commands\SetupCommand;
 
 class LaraJSCoreServiceProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class LaraJSCoreServiceProvider extends ServiceProvider
             $this->commands([
                 GenerateActionCommand::class,
                 GenerateControllerCommand::class,
+                GenerateRepositoryCommand::class,
                 SetupCommand::class,
             ]);
         }
